7b1306041e96a463afa4beb087e4d75e0f5bbc35
Jonathan Lee <jonnylee719@gmail.com>
2015-04-19 22:04:51 +0800

    Start on project

---
+++ src/tool/SelectionSorter.java
@@ -0,0 +1,34 @@
+/**
+ * CSCI1530 Java Programming Assignment
+ *
+ * --- Declaration ---
+ *
+ * I declare that the assignment here submitted is original except for source
+ * material explicitly acknowledged. I also acknowledge that I am aware of
+ * University policy and regulations on honesty in academic work, and of the
+ * disciplinary guidelines and procedures applicable to breaches of such policy
+ * and regulations, as contained in the website
+ *
+ *     http://www.cuhk.edu.hk/policy/academichonesty/
+ *
+ * Assignment 6
+ * Student Name : Lee Wai Lam Jonathan
+ * Student ID   : 1155017574
+ * Email Address: s1155017574@cuhk.edu.hk
+ 
+
+ * Date    : 14/04/2015
+ */
+package tool;
+
+/**
+ *
+ * @author user
+ */
+public class SelectionSorter {
+    
+    // This objects uses selection sort to sort an array
+    
+    
+    
+}
